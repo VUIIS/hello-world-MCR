c50099c2fb43fc232f742b336b06cacc600e7c78
lawlessrd <46003947+lawlessrd@users.noreply.github.com>
2021-03-10 12:10:24 -0600

    Add files via upload

--- hello_world_master/test_sing_container.sh
+++ hello_world_master/test_sing_container.sh
@@ -12,16 +12,3 @@ filename hello_world.txt \
 in_dir /INPUTS \
 out_dir /OUTPUTS
 
-
-
-singularity run \
---cleanenv \
---contain \
---home ${SINGULARITY_ROOTFS}/INPUTS \
---bind INPUTS:/INPUTS \
---bind OUTPUTS:/OUTPUTS \
-hello_world.simg \
-display_string "Hello, world!" \
-filename hello_world.txt \
-in_dir /INPUTS \
-out_dir /OUTPUTS
