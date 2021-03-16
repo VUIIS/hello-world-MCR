21589f3c19654e9c0745a8241efce4215f9773ac
lawlessrd <46003947+lawlessrd@users.noreply.github.com>
2021-03-16 09:36:26 -0500

    Add files via upload

---
+++ test_sing_container.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+singularity run \
+--cleanenv \
+--contain \
+--home $(pwd -P)/INPUTS \
+--bind $(pwd -P)/INPUTS:/INPUTS \
+--bind $(pwd -P)/OUTPUTS:/OUTPUTS \
+test.simg \
+display_string "Hello, world!" \
+filename hello_world.txt \
+in_dir /INPUTS \
+out_dir /OUTPUTS
+
+
